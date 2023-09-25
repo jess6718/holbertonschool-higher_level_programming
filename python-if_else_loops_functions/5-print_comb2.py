1453d0388fb72002ebd63857e5c340f0a0eca80b
jess6718 <6718@holbertonstudents.com>
2023-09-25 08:46:07 +0200

    test

--- python-if_else_loops_functions/5-print_comb2.py
+++ python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-for number in range(0, 100):
-    if number < 99:
-        print("{:02}, ".format(number), end="")
+for value in range (0, 100):
+    if value <= 98:
+        print("{:02d}, ".format(value), end="")
     else:
-        print("{:02}".format(number))
+        print("{:02d}".format(value))
