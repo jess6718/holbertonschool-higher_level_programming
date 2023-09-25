9a22acda6f113ea11414797905c572bbf4a27baa
jess6718 <6718@holbertonstudents.com>
2023-09-25 13:24:27 +0200

    feat: add task 9

---
+++ python-if_else_loops_functions/9-print_last_digit.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def print_last_digit(number):
+    if number >= 0:
+        last_d = number % 10
+    else:
+        last_d = abs(number % -10)
+    print("{}".format(last_d), end="")
+    return last_d
