2d57526eb3ee7cce30cb327cf21f7f6a15fed3bf
jess6718 <6718@holbertonstudents.com>
2023-09-26 07:19:33 +0200

    feat: add task 2

---
+++ python-import_modules/2-args.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+import sys
+if __name__ == '__main__':
+
+    arg_c = len(sys.argv) - 1
+    if arg_c > 1:
+        print("{} argument:".format(arg_c))
+    else:
+        print("{} arguments.".format(arg_c))
+    i = 1
+    while i <= arg_c and arg_c > 0:
+        print("{}: {}".format(i, sys.argv[i]))
+        i = i + 1
