5e8869ee5b709fdfb03b1355e948681d37416ccb
jess6718 <6718@holbertonstudents.com>
2023-09-26 11:26:20 +0200

    final

--- python-import_modules/3-infinite_add.py
+++ python-import_modules/3-infinite_add.py
@@ -2,10 +2,7 @@
 import sys
 
 if __name__ == '__main__':
-    arg_c = len(sys.argv) - 1
-    i = 1 
     total = 0
-    while i <= arg_c:
+    for i in range(1, len(sys.argv)):
         total = total + int(sys.argv[i])
-        i = i + 1
     print(f"{total}")
