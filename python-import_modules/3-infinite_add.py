7f5beb7b8cae4d0927ea6fdbf00e29d2b390708f
jess6718 <6718@holbertonstudents.com>
2023-09-26 11:08:09 +0200

    feat: add task 3

---
+++ python-import_modules/3-infinite_add.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+import sys
+if __name__ == '__main__':
+    arg_c = len(sys.argv) - 1
+    i = 1 
+    total = 0
+    while i <= arg_c:
+        total = total + int(sys.argv[i])
+        i = i + 1
+    print("{}".format(total))
