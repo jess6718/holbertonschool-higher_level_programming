658db0df13adbbdab9e1b535a0751c13e4299bc8
jess6718 <6718@holbertonstudents.com>
2023-09-26 04:41:25 +0200

    fix: keep only two variables

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -3,5 +3,4 @@ from add_0 import add
 if __name__ == '__main__': #entry point of this program
     a = 1
     b = 2
-    c = add(a, b)
-    print("{0} + {1} = {2}".format(a, b, c))
+    print("{0} + {1} = {2}".format(a, b, add(a, b)))
