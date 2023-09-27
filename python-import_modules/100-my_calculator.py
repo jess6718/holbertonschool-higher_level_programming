1c1863d32ed84806677e512e19d09fb2a54dcfa5
jess6718 <6718@holbertonstudents.com>
2023-09-27 07:39:17 +0200

    fix: change line of entry point

--- python-import_modules/100-my_calculator.py
+++ python-import_modules/100-my_calculator.py
@@ -1,22 +1,26 @@
 #!/usr/bin/python3
-from calculator_1 import add, sub, mul, div
-import sys
 if __name__ == '__main__':
+    from calculator_1 import add, sub, mul, div
+    import sys
+
     if len(sys.argv) != 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         exit (1)
+
     a = int(sys.argv[1])
     b = int(sys.argv[3])
     op = sys.argv[2]
+
     if op == "+":
-        cal = add(a, b)
+        calculation = add(a, b)
     elif op == "-":
-        cal = sub(a, b)
+        calculation = sub(a, b)
     elif op == "*":
-        cal = mul(a, b)
+        calculation = mul(a, b)
     elif op == "/":
-        cal = div(a, b)
+        calculation = div(a, b)
     else:
         print("Unknown operator. Available operators: +, -, * and /")
         exit (1)
-    print("{} {} {} = {}".format(a, op, b, cal))
+        
+    print("{} {} {} = {}".format(a, op, b, calculation))
