e0afc3514cf91313e8f16f0caeadfc4c41bb37a8
jess6718 <6718@holbertonstudents.com>
2023-09-27 07:41:32 +0200

    fix: remove space

--- python-import_modules/100-my_calculator.py
+++ python-import_modules/100-my_calculator.py
@@ -5,7 +5,7 @@ if __name__ == '__main__':
 
     if len(sys.argv) != 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
-        exit (1)
+        exit(1)
 
     a = int(sys.argv[1])
     b = int(sys.argv[3])
@@ -21,6 +21,6 @@ if __name__ == '__main__':
         calculation = div(a, b)
     else:
         print("Unknown operator. Available operators: +, -, * and /")
-        exit (1)
-        
+        exit(1)
+
     print("{} {} {} = {}".format(a, op, b, calculation))
