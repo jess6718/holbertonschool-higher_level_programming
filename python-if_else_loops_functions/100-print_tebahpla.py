a7c2ede1ef3ba893230d2e5e25de667953be12c7
jess6718 <6718@holbertonstudents.com>
2023-09-27 03:02:42 +0200

    fix: switch f-string to .format

--- python-if_else_loops_functions/100-print_tebahpla.py
+++ python-if_else_loops_functions/100-print_tebahpla.py
@@ -3,5 +3,4 @@ for ascii in range(122, 96, -1):
     letter = chr(ascii)
     if ascii % 2 != 0:
         letter = chr(ascii - 32)
-    print(f"{letter}", end="")
-        
+    print("{}".format(letter), end="")
