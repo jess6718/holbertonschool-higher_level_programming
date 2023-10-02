0f346a74a18cbcd8e11aa97203671816a6df946f
jess6718 <6718@holbertonstudents.com>
2023-10-02 10:19:07 +0200

    feat: add task 9

---
+++ python-data_structures/9-max_integer.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def max_integer(my_list=[]):
+    if my_list is None:
+        return None
+    for i in range(len(my_list) - 1):
+        if my_list[i] > my_list[i + 1]:
+            a = my_list[i]
+            my_list[i] = my_list[i + 1]
+            my_list[i + 1] = a
+    return my_list[i + 1]
