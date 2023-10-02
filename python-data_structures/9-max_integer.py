ac87f55376301813f6364e3c6890fd95c27d591f
jess6718 <6718@holbertonstudents.com>
2023-10-02 10:30:41 +0200

    edge case

--- python-data_structures/9-max_integer.py
+++ python-data_structures/9-max_integer.py
@@ -2,6 +2,10 @@
 def max_integer(my_list=[]):
     if my_list is None:
         return None
+
+    if len(my_list) == 1:
+        return my_list[0]
+
     for i in range(len(my_list) - 1):
         if my_list[i] > my_list[i + 1]:
             temp = my_list[i]
