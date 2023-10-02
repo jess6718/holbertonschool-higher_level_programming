27fecde56bd0716f39bed2c4d29d0aa63b6f87cc
jess6718 <6718@holbertonstudents.com>
2023-10-02 08:08:06 +0200

    fix: indentation

--- python-data_structures/8-multiple_returns.py
+++ python-data_structures/8-multiple_returns.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
     result = (len(sentence), sentence[0])
-    if len(sentence) > 0:
+    if len(sentence) == 0:
         result = (0, None)
     return result
