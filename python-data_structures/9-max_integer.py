ae9b01dccbd759d2f8e8cf835fbd0bba134cce4d
jess6718 <6718@holbertonstudents.com>
2023-10-02 10:25:14 +0200

    fix: i + 1 out of index

--- python-data_structures/9-max_integer.py
+++ python-data_structures/9-max_integer.py
@@ -2,7 +2,7 @@
 def max_integer(my_list=[]):
     if my_list is None:
         return None
-    for i in range(len(my_list) - 1):
+    for i in range(len(my_list) - 2):
         if my_list[i] > my_list[i + 1]:
             a = my_list[i]
             my_list[i] = my_list[i + 1]
