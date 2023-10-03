37e2370bd2ff6009360ccc5faa0c3f85098d2721
jess6718 <6718@holbertonstudents.com>
2023-10-03 12:29:52 +0200

    feat: add task 7

---
+++ python-more_data_structures/7-update_dictionary.py
@@ -0,0 +1,9 @@
+def update_dictionary(a_dictionary, key, value):
+    key_list = list(a_dictionary.keys())
+
+    for x in key_list:
+        if x == key:
+            a_dictionary.update({key: value})
+        else:
+            a_dictionary[key] = value
+    return a_dictionary
