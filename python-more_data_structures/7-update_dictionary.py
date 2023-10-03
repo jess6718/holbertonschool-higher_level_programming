46a5e4d9d9725f489709685000622b35324275ee
jess6718 <6718@holbertonstudents.com>
2023-10-03 12:35:15 +0200

    fix: edge case

--- python-more_data_structures/7-update_dictionary.py
+++ python-more_data_structures/7-update_dictionary.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
 def update_dictionary(a_dictionary, key, value):
+    if a_dictionary is None or len(a_dictionary) == 0:
+        a_dictionary[key] = value
+        return a_dictionary
     key_list = list(a_dictionary.keys())
 
     for x in key_list:
