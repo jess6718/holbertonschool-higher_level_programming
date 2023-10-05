c99c1ba805800647952b7face09b6c0a0f871b06
jess6718 <6718@holbertonstudents.com>
2023-10-05 12:32:25 +0200

    feat fix style

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -4,15 +4,15 @@ def roman_to_int(roman_string):
         return 0
 
     roamn_dict = {
-        "I" : 1,
-        "V" : 5,
-        "X" : 10,
-        "L" : 50,
-        "C" : 100,
-        "D" : 500,
-        "M" : 1000
+        "I": 1,
+        "V": 5,
+        "X": 10,
+        "L": 50,
+        "C": 100,
+        "D": 500,
+        "M": 1000
         }
-    
+
     total = 0
     pre_val = 0
 
