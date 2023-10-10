0f4db27349b7019426fdd278772a1215a9e92d2d
jess6718 <6718@holbertonstudents.com>
2023-10-10 14:11:17 +0200

    feat: task 6

---
+++ python-classes/6-square.py
@@ -0,0 +1,49 @@
+#!/usr/bin/python3
+
+
+"""Define an class Square."""
+
+
+class Square:
+    """Define a private instance size."""
+
+    def __init__(self, size=0, position=(0, 0)):
+        self.__size = size
+        self.__position = position
+
+    @property
+    def size(self):
+        return self.__size
+
+    @property
+    def position(self):
+        return self.__position
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    @position.setter
+    def position(self, value):
+        if not isinstance(value[0], int) or not isinstance(value[1], int):
+            raise TypeError("position must be a tuple of 2 positive integers")
+        if value[0] < 0 or value[1] < 0:
+            raise TypeError("position must be a tuple of 2 positive integers")
+
+    def area(self):
+        area = self.__size * self.__size
+        return area
+
+    def my_print(self):
+        if self.size == 0:
+            print()
+        else:
+            for count in range(0, self.position[1]):
+                print("")
+            for i in range(0, self.size):
+                print(" " * self.position[0], end="")
+                print("#" * self.size)
