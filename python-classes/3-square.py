c488f7710e0a57056cda1a4489b81699ed348d04
jess6718 <6718@holbertonstudents.com>
2023-10-10 07:53:24 +0200

    feat: add task 3

---
+++ python-classes/3-square.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+
+"""Define an class Square."""
+
+
+class Square:
+    """Define a private instance size."""
+
+    def __init__(self, size=0):
+        self.__size = size
+
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+
+    def area(self):
+        area = self.__size * self.__size
+        return area
