813f9e40222b3c816d323312fcec4726b2cfae0f
jess6718 <6718@holbertonstudents.com>
2023-10-11 12:35:20 +0200

    feat: add task 0

---
+++ python-test_driven_development/0-add_integer.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""A function that adds 2 integers"""
+
+
+def add_integer(a, b=98):
+    """Adding edge cases"""
+
+    
+    if not isinstance(a, int):
+        if not isinstance(a, float):
+            raise TypeError("a must be an integer")
+    if not isinstance(b, int):
+        if not isinstance(b, float):
+            raise TypeError("b must be an integer")
+    if isinstance(a, float):
+        a = int(a)
+    if isinstance(b, float):
+        b = int(b)
+    return a + b
