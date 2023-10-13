4f2e6c7e0ab08ea937a83ffcaa823672d5e2a29b
jess6718 <6718@holbertonstudents.com>
2023-10-13 07:52:55 +0200

    fix: add documents and 1 negative test

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -1,5 +1,10 @@
 #!/usr/bin/python3
+"""A module that print name"""
+
+
 def say_my_name(first_name, last_name=""):
+    """A function that print name"""
+
     if not isinstance(first_name, str):
         raise TypeError("first_name must be a string")
     if not isinstance(last_name, str):
