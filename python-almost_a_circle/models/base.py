d9e710a67ce8772fc17dbfb6b4573bae63c09013
jess6718 <6718@holbertonstudents.com>
2023-10-25 14:01:13 +0200

    feat: add task 17

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -16,6 +16,14 @@ class Base:
 
     @staticmethod
     def to_json_string(list_dictionaries):
+        """Return JSON representation"""
         if list_dictionaries is None or list_dictionaries == []:
             return "[]"
         return json.dumps(list_dictionaries)
+
+    @staticmethod
+    def from_json_string(json_string):
+        """Return str representation"""
+        if json_string is None or json_string == "":
+            return []
+        return json.loads(json_string)
