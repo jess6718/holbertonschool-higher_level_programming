31358a21c925eb0060e712fc8b210fc4ac113962
jess6718 <6718@holbertonstudents.com>
2023-10-25 12:51:59 +0200

    docs: add comments

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -7,9 +7,13 @@ class Square(Rectangle):  # inherite all attribute/var from Rectangle
     """Subclass class init"""
 
     def __init__(self, size, x=0, y=0, id=None):
+        """Initiate instance var"""
+
         # can also use key:value format "width=size"
         super().__init__(size, size, x, y, id)
 
     def __str__(self):  # each class has its own __str__
+        """print str"""
+
         return f"[Square] ({self.id}) {self.x}/{self.y} - "\
             f"{self.width}"
