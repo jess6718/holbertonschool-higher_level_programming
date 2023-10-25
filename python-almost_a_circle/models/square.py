1309ec7ab5f085c7361d5282ec8b38ed11526dd8
jess6718 <6718@holbertonstudents.com>
2023-10-25 12:47:14 +0200

    fix: pycodestyle

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -3,7 +3,7 @@ from models.rectangle import Rectangle
 """Define Sub Class square"""
 
 
-class Square(Rectangle):  #inherite all attribute/var from Rectangle
+class Square(Rectangle):  # inherite all attribute/var from Rectangle
     def __init__(self, size, x=0, y=0, id=None):
         # can also use key:value format "width=size"
         super().__init__(size, size, x, y, id)
@@ -11,4 +11,3 @@ class Square(Rectangle):  #inherite all attribute/var from Rectangle
     def __str__(self):  # each class has its own __str__
         return f"[Square] ({self.id}) {self.x}/{self.y} - "\
             f"{self.width}"
-    
