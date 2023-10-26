7d4d7a8032b4bd5a2a8ed94a7e4ca814ed2cc83c
jess6718 <6718@holbertonstudents.com>
2023-10-26 04:52:25 +0200

    feat: add task 16

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -16,14 +16,27 @@ class Base:
 
     @staticmethod
     def to_json_string(list_dictionaries):
-        """Return JSON representation"""
+        """Return Json str"""
         if list_dictionaries is None or list_dictionaries == []:
             return "[]"
         return json.dumps(list_dictionaries)
 
     @staticmethod
     def from_json_string(json_string):
-        """Return str representation"""
+        """Return Json dictionary"""
         if json_string is None or json_string == "":
             return []
         return json.loads(json_string)
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """Define a function that write object to a text file"""
+        file_name = cls.__name__ + ".json"
+        dict_list = []
+        with open(file_name, "w") as file:  # file is a var for opened file
+            if list_objs is None:
+                file.write("[]")
+            else:
+                for item in list_objs:
+                    dict_list.append(item.to_dictionary())
+                file.write(Base.to_json_string(dict_list))
