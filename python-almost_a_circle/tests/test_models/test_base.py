436d6c1c0d53e4cc73ae0fa79a6fe7df7e4c78d2
jess6718 <6718@holbertonstudents.com>
2023-10-27 03:31:42 +0200

    feat: add test for base__init__

--- python-almost_a_circle/tests/test_models/test_base.py
+++ python-almost_a_circle/tests/test_models/test_base.py
@@ -8,5 +8,12 @@ class TestBaseClass(unittest.TestCase):
     """Test methods of BassClass"""
 
     def test_init_none_id(self):
-        base = Base()
-        self.assertEqual(base.id, 1)
+        base1 = Base()
+        self.assertEqual(base1.id, 1)
+        base2 = Base()
+        self.assertEqual(base2.id, 2)
+        self.assertEqual(base1.id, 1)
+
+    def test_init_id_passed(self):
+        base = Base(7)
+        self.assertEqual(base.id, 7)
