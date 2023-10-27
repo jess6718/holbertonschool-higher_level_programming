757faa6f8b65f34c791101d08107ffca84cdf5ad
jess6718 <6718@holbertonstudents.com>
2023-10-27 03:48:03 +0200

    include update test

--- python-almost_a_circle/tests/test_models/test_base.py
+++ python-almost_a_circle/tests/test_models/test_base.py
@@ -6,17 +6,23 @@ from models.base import Base
 class TestBaseClass(unittest.TestCase):
     """Test methods of BassClass"""
 
+    def setUp(self):
+        """Runs for each test reset private attribute"""
+        Base._Base__nb_objects = 0
+
     def test_init_none_id(self):
-        """Test whether incremental works none_id passed in"""
-        base1 = Base()
-        self.assertEqual(base1.id, 1)
-        base2 = Base()
-        self.assertEqual(base2.id, 2)
-        self.assertEqual(base1.id, 1)
-        self.assertNotEqual(base1.id, base2.id)
-        self.assertEqual(base1.id+1, base2.id)
+        """Test none_id passed in"""
+        base0 = Base()
+        self.assertEqual(base0.id, 1)
 
     def test_init_id_passed(self):
         """Test whether Base returns id if a number passed in"""
         base = Base(7)
         self.assertEqual(base.id, 7)
+
+    def test_init_increm_id(self):
+        """Test whether incremental works none_id passed in"""
+        base1 = Base()
+        base2 = Base()
+        self.assertEqual(base1.id, 1)
+        self.assertEqual(base2.id, 2)
