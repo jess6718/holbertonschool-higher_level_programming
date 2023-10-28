a3da2f093a479825e064f1d55bbd8f6c5c2235ec
jess6718 <6718@holbertonstudents.com>
2023-10-29 01:23:10 +0200

    test base

--- python-almost_a_circle/tests/test_models/test_base.py
+++ python-almost_a_circle/tests/test_models/test_base.py
@@ -1,80 +1,43 @@
 #!/usr/bin/python3
-"""
-Unittest for Base module
-"""
+"""Test Module"""
 import unittest
 from models.base import Base
-import io
-import sys
-import os
 
 
-class TestBase(unittest.TestCase):
-    """
-    Tests for Base Class
-    """
-    def test_base_arguments(self):
-        """
-        Testing for missing arguments
-        """
-        base_1 = Base()
-        self.assertEqual(base_1.id, 1)
-        base_1_compare = base_1.id > 0
-        self.assertEqual(base_1_compare, True)
-        base_2 = Base()
-        self.assertEqual(base_1.id + 1, base_2.id)
-        base_3 = Base(2)
-        self.assertEqual(base_3.id, 2)
+class TestBaseClass(unittest.TestCase):
+    """Test methods of BassClass"""
 
-    def test_to_json_string(self):
-        """
-        Test with an empty list
-        """
-        base_data_1 = []
-        json_string_1 = Base.to_json_string(base_data_1)
-        self.assertEqual(json_string_1, "[]")
+    def setUp(self):
+        """Runs for each test reset private attribute"""
+        Base._Base__nb_objects = 0
 
-    def test_to_json_string_output(self):
-        """
-        Test for correct output of JSON string representation
-        """
-        base_data_2 = [{"M": "1"}, {"N": "2"}, {"J": "3"}]
-        json_string_2 = Base.to_json_string(base_data_2)
-        self.assertEqual(json_string_2, '[{"M": "1"}, {"N": "2"}, {"J": "3"}]')
+    def test_init_none_id(self):
+        """Test none_id passed in"""
+        base0 = Base()
+        self.assertEqual(base0.id, 1)
 
-    def test_to_json_string_none(self):
-        """
-        Test for correct output of JSON string representation if the input
-        is 'None'
-        """
-        base_data_3 = None
-        json_string_3 = Base.to_json_string(base_data_3)
-        self.assertEqual(json_string_3, '[]')
+    def test_init_id_passed(self):
+        """Test whether Base returns id if a number passed in"""
+        base = Base(7)
+        self.assertEqual(base.id, 7)
 
-    def test_from_json_string_input(self):
-        """
-        Test for string input
-        """
-        json_string_1 = '[{"M": "1"}, {"N": "2"}, {"J": "3"}]'
-        base_data_1 = Base.from_json_string(json_string_1)
-        self.assertEqual(base_data_1, [{"M": "1"}, {"N": "2"}, {"J": "3"}])
+    def test_init_increm_id(self):
+        """Test whether incremental works none_id passed in"""
+        base1 = Base()
+        base2 = Base()
+        self.assertEqual(base1.id, 1)
+        self.assertEqual(base2.id, 2)
 
-    def test_from_json_string_empty(self):
-        """
-        Test for empty string input
-        """
-        json_string_2 = ''
-        base_data_2 = Base.from_json_string(json_string_2)
-        self.assertEqual(base_data_2, [])
+    def test_json_string_with_none(self):
+        """Test Json string with None passed in"""
+        self.assertEqual(Base.to_json_string(None), "[]")
 
-    def test_from_json_string_none(self):
-        """
-        Test for None string input
-        """
-        json_string_3 = None
-        base_data_3 = Base.from_json_string(json_string_3)
-        self.assertEqual(base_data_3, [])
+    def test_json_string_with_empty_list(self):
+        """Test Json string with None passed in"""
+        self.assertEqual(Base.to_json_string([]), "[]")
 
-
-if __name__ == '__main__':
-    unittest.main()
+    def test_json_string_with_dict_list(self):
+        """Test Json string with valid dict_list"""
+        list_input = [{'id': 89}, {'width': 10}]
+        self.assertEqual(Base.to_json_string(list_input),
+                         '[{"id": 89}, {"width": 10}]')
