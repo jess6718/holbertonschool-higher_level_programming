4d8acd057634a46be9d23f66409bdfbbdfc38c45
jess6718 <6718@holbertonstudents.com>
2023-10-29 10:38:22 +0100

    feat: add square test

--- python-almost_a_circle/tests/test_models/test_square.py
+++ python-almost_a_circle/tests/test_models/test_square.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """Module for testing Square class"""
 import unittest
+import os
 from models.base import Base
 from models.square import Square
 
@@ -62,13 +63,56 @@ class TestSquare(unittest.TestCase):
         prt_str2 = Square(3, 0)
         self.assertEqual(str(prt_str2), "[Square] (1) 0/0 - 3")
 
+    def test_update(self):
+        """Test update"""
+        s1 = Square(1, 2, 3, 4)
+        s1.update(7)
+        self.assertEqual(s1.id, 7)
+
+        s2 = Square(1, 2)
+        s2.update()
+        self.assertEqual(s2.size, 1)
+        self.assertEqual(s2.x, 2)
+
     def test_to_dictionary(self):
         """Testing to_dictionary()"""
         s1 = Square(1, 2, 3, 4)
         result = s1.to_dictionary()
         self.assertEqual(result, {"id": 4, "size": 1, "x": 2, "y": 3})
 
-    # def test_update(self):
-    # def test_create(self):
-    # def test_save_to_file(self):
-    # def test_from_json(self):
+    def test_create(self):
+        """Test create"""
+        s1 = Square.create(**{'id': 3, 'size': 1, 'x': 3, 'y': 4})
+        expect = "[Square] (3) 3/4 - 1"
+        self.assertEqual(str(s1), expect)
+
+    def test_save_to_file(self):
+        """Test save to file"""
+        Square.save_to_file(None)
+        with open("Square.json", "r") as t_file:
+            self.assertEqual(t_file.read(), "[]")
+            os.remove("Square.json")
+
+        Square.save_to_file([])
+        with open("Square.json", "r") as t_file:
+            self.assertEqual(t_file.read(), "[]")
+            os.remove("Square.json")
+
+        Square.save_to_file([Square(1, 2)])
+        with open("Square.json", "r") as t_file:
+            expect = '[{"id": 1, "size": 1, "x": 2, "y": 0}]'
+            self.assertEqual(t_file.read(), expect)
+            os.remove("Square.json")
+
+    def test_from_json(self):
+        """Test load from file"""
+        s1 = Square(1)
+        s2 = Square(3)
+        list_of_objs = [s1, s2]
+        Square.save_to_file(list_of_objs)
+
+        result_list = Square.load_from_file()
+        self.assertTrue(len(result_list) == 2)
+        self.assertTrue(result_list[0].size == 1)
+        self.assertTrue(result_list[1].size == 3)
+        os.remove("Square.json")
