6d426951b95664fbee93c5ce5637f848180de74a
jess6718 <6718@holbertonstudents.com>
2023-11-21 05:56:17 +0100

    feat: add task 9

---
+++ SQL_more_queries/9-cities_by_state_join.sql
@@ -0,0 +1,6 @@
+-- script that lists all cities contained in the database hbtn_0d_usa
+
+SELECT cities.id, cities.name, states.name
+FROM cities
+RIGHT JOIN states ON cities.state.id = states.id
+ORDER BY cities.id ASC;
